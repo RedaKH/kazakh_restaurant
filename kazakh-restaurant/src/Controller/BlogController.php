98d58ecd50365c7784f29e54948a4e9414a0418f
RedaKH <37907191+RedaKH@users.noreply.github.com>
2024-09-02 17:40:45 +0200

    implements-blog-feature

--- kazakh-restaurant/src/Controller/BlogController.php
+++ kazakh-restaurant/src/Controller/BlogController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
@@ -20,7 +21,7 @@ class BlogController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-    #[Route('/employe/makeblog', name: 'make_blog')]
+    #[Route('/blog/new', name: 'make_blog')]
     public function make_blog(Request $request): Response
     {
         $blog = new Blog();
@@ -60,4 +61,89 @@ class BlogController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/article/{id}', name: 'blog_read')]
+ 
+
+    public function read_blog(Blog $blog){
+        return $this->render('blog/read_blog.html.twig',[
+            'blog'=>$blog
+        ]);
+    }
+
+    #[Route('/articles', name: 'blogs')]
+
+    public function blogs(Blog $blog){
+        return $this->render('blog/read_blog.html.twig',[
+            'blogs'=>$blog
+        ]);
+    }
+
+
+
+    #[Route('/blog/{id}/edit', name: 'blog_edit')]
+ 
+
+    public function edit_blog(Request $request,Blog $blog){
+        
+        $blog = new Blog();
+        $form = $this->createForm(BlogType::class,$blog);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(),PATHINFO_FILENAME);
+                $newFileName = uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    //throw $th;
+                }
+                //insere l'image a l'entite
+
+                $blog->setImage($newFileName);
+
+
+                // Sauvegarde le blog dans la base de données
+            $this->entityManager->persist($blog);
+            $this->entityManager->flush();
+        
+            return $this->redirectToRoute('blog', ['id' => $blog->getId()]);
+            }
+
+
+            return $this->render('blog/edit.html.twig', [
+                'form' => $form->createView(),
+                'blog'=> $blog
+            ]);
+
+
+
+
+    }
+
+    
+    
+
 }
+#[Route('/blog/{id}/delete', name: 'blog_delete')]
+
+    public function delete_blog(Blog $blog): Response
+    {
+        $this->entityManager->remove($blog);
+        $this->entityManager->flush();
+    
+        return $this->redirectToRoute('blogs');
+    }
+
+}
+
+
+
+
